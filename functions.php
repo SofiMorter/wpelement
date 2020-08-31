4fe6634bc62258ccb2487374dc47213320d951b8
SofiMorter <sofimorter@gmail.com>
2020-09-01 01:03:27 +0600

    add woo support

--- functions.php
+++ functions.php
@@ -190,10 +190,8 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-<?php
-function customtheme_add_woocommerce_support()
- {
-add_theme_support( 'woocommerce' );
+function customtheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
 }
+
 add_action( 'after_setup_theme', 'customtheme_add_woocommerce_support' );
-?>
--- woocommerce.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-
-<?php woocommerce_content(); ?>
-
-
-<?php get_footer(); ?>
