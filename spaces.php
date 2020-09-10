876192d5efabb2239d81c92da7452af64bdf292b
SofiMorter <sofimorter@gmail.com>
2020-09-10 23:22:07 +0600

    spaces links

--- spaces.php
+++ spaces.php
@@ -7,89 +7,89 @@ Template Name: Пространства
 
 
 
- <div class="page-content-wrapper">
-        <section class="spaces-first">
-          <div>
-            Выберите пространство<br>для декорирования
-          </div>
-          <ul>
-            <li><a href="http://">Гостиная</a></li>
-            <li><a href="http://">Комната для взрослых</a></li>
-            <li><a href="http://">Детская</a></li>
-            <li><a href="http://">Ванная комната</a></li>
-            <li><a href="http://">Кухня</a></li>
-            <li><a href="http://">Гараж</a></li>
-            <li><a href="http://">Офис</a></li>
-            <li><a href="http://">Коммерческие помещения</a></li>
-            <li><a href="http://">Для наружных работ</a></li>
-          </ul>
-        </section>
-        <section class="space-grid">
-          <div class="space-grid_row">
-            <div class="space-grid_row-big">
-              <a href="http://#" class="space-grid_a"> 
-                <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img1')['url']; ?>);">
-                <span>Гостиная</span></div>
-              </a>
-            </div>
-            <div class="space-grid_row-triple">
-              <div class="space-grid_row-triple_big">
-                <a href="http://#" class="space-grid_a">
-                  <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img2')['url']; ?>);">
-                <span>Комната для взрослых</span></div>
-                </a>
-              </div>
-              <div class="space-grid_row-triple_smol">
-                <a href="http://#" class="space-grid_a">
-                  <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img3')['url']; ?>);">
-                <span>Детская комната</span></div>
-                </a>
-              </div>
-              <div class="space-grid_row-triple_smol">
-                <a href="http://#" class="space-grid_a">
-                  <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img4')['url']; ?>);">
-                <span>Ванная комната</span></div>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="space-grid_row">
-            <a href="http://#" class="space-grid_a">
-              <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img5')['url']; ?>);">
-            <span>Кухня</span></div>
-            </a>
-          </div>
-          <div class="space-grid_row">
-           
-            <div class="space-grid_row-triple">
-              <div class="space-grid_row-triple_big">
-                <a href="http://#" class="space-grid_a">
-                  <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img6')['url']; ?>);">
-                <span>Гараж</span></div>
-                </a>
-              </div>
-              <div class="space-grid_row-triple_smol">
-                <a href="http://#" class="space-grid_a">
-                  <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img7')['url']; ?>);">
-                <span>Офис</span></div>
-                </a>
-              </div>
-              <div class="space-grid_row-triple_smol">
-                <a href="http://#" class="space-grid_a">
-                  <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img8')['url']; ?>);">
-                <span>Коммерческие помещения</span></div>
-                </a>
-              </div>
-            </div>
-            <div class="space-grid_row-big">
-                <a href="http://#" class="space-grid_a">
-                  <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img9')['url']; ?>);">
-                <span>Для наружных работ</span></div>
-                </a>
-              </div>
-          </div>
-        </section>
+<div class="page-content-wrapper">
+  <section class="spaces-first">
+    <div>
+      Выберите пространство<br>для декорирования
+    </div>
+    <ul>
+      <li><a href="http://element1/spaces/gostinaya/">Гостиная</a></li>
+      <li><a href="http://element1/spaces/bedroom/">Спальня</a></li>
+      <li><a href="http://element1/spaces/detskaya/">Детская</a></li>
+      <li><a href="http://element1/spaces/vannaya/">Ванная комната</a></li>
+      <li><a href="http://element1/spaces/kuhnya/">Кухня</a></li>
+      <li><a href="http://element1/spaces/garazh/">Гараж</a></li>
+      <li><a href="http://element1/spaces/ofis/">Офис</a></li>
+      <li><a href="http://element1/spaces/kommercheskie-pomeshheniya/">Коммерческие помещения</a></li>
+      <li><a href="http://element1/spaces/fasady/">Для наружных работ</a></li>
+    </ul>
+  </section>
+  <section class="space-grid">
+    <div class="space-grid_row">
+      <div class="space-grid_row-big">
+        <a href="http://element1/spaces/gostinaya/" class="space-grid_a">
+          <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img1')['url']; ?>);">
+            <span>Гостиная</span></div>
+        </a>
       </div>
+      <div class="space-grid_row-triple">
+        <div class="space-grid_row-triple_big">
+          <a href="http://element1/spaces/bedroom/" class="space-grid_a">
+            <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img2')['url']; ?>);">
+              <span>Спальня</span></div>
+          </a>
+        </div>
+        <div class="space-grid_row-triple_smol">
+          <a href="http://element1/spaces/detskaya/" class="space-grid_a">
+            <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img3')['url']; ?>);">
+              <span>Детская комната</span></div>
+          </a>
+        </div>
+        <div class="space-grid_row-triple_smol">
+          <a href="http://element1/spaces/vannaya/" class="space-grid_a">
+            <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img4')['url']; ?>);">
+              <span>Ванная комната</span></div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="space-grid_row">
+      <a href="http://element1/spaces/kuhnya" class="space-grid_a">
+        <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img5')['url']; ?>);">
+          <span>Кухня</span></div>
+      </a>
+    </div>
+    <div class="space-grid_row">
+
+      <div class="space-grid_row-triple">
+        <div class="space-grid_row-triple_big">
+          <a href="http://element1/spaces/garazh/" class="space-grid_a">
+            <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img6')['url']; ?>);">
+              <span>Гараж</span></div>
+          </a>
+        </div>
+        <div class="space-grid_row-triple_smol">
+          <a href="http://element1/spaces/ofis/" class="space-grid_a">
+            <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img7')['url']; ?>);">
+              <span>Офис</span></div>
+          </a>
+        </div>
+        <div class="space-grid_row-triple_smol">
+          <a href="http://element1/spaces/kommercheskie-pomeshheniya/" class="space-grid_a">
+            <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img8')['url']; ?>);">
+              <span>Коммерческие помещения</span></div>
+          </a>
+        </div>
+      </div>
+      <div class="space-grid_row-big">
+        <a href="http://element1/spaces/fasady/" class="space-grid_a">
+          <div class="box-img" style="background-image: url(<?php echo get_field('spaces_img9')['url']; ?>);">
+            <span>Для наружных работ</span></div>
+        </a>
+      </div>
+    </div>
+  </section>
+</div>
 
 
 
